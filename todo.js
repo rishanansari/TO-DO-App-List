fb7b7c671ac17886fae9030f604139145f04b36c
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-06-07 16:41:11 +0530

    done...

--- todo.js
+++ todo.js
@@ -1,8 +1,23 @@
-let button = document.querySelector('#btn');
-button.addEventListener('click', function(){
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', function(){
     
-    let input = document.querySelector('#input').value;
+    var input = document.querySelector('#input').value;
     document.querySelector('#input').value = '';
+
+    var li = document.createElement('li');
+    li.className = 'm-3';
+    li.textContent = input;
+
+    var listbtn = document.createElement('button');
+    listbtn.textContent = 'Delete'
+    listbtn.className = 'btn btn-danger ms-3 btn-sm';
+    listbtn.addEventListener('click', () => li.remove());
+    li.appendChild(listbtn);   
+    // document.querySelector('.list').appendChild(listbtn); can not use this lineof code after giving className as list.
+
+    document.querySelector('.addlist').appendChild(li);
     
+    
+
     console.log(input);
 })
